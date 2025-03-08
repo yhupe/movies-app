df8733bf2e0911480991762bf4dd0529c370b445
yhupe <hannes.pickel@outlook.com>
2025-03-08 20:37:09 +0100

    adding requirements.txt file

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from movie_app import MovieApp
 from storage_json import StorageJson
 from storage_csv import StorageCSV
 
+
+# Instantiating storage files for .csv / .json
 storage_json = StorageJson('movies.json')
 storage_csv = StorageCSV('movies.csv')
+
+# Instantiating movie apps based on the storage file created earlier
 movie_app_json = MovieApp(storage_json)
 movie_app_csv = MovieApp(storage_csv)
 
-
+# Calling MovieApp.run() to start the application
 movie_app_csv.run()
 
 
